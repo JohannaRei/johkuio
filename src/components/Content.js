8c4c567a02508d6ac4bdedc78d667432f50e69e0
Johanna Reinikainen <johanna.reinikainen@hiq.fi>
2019-03-14 15:41:07 +0200

    Styling adjustments

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,6 +1,6 @@
 // @flow
 import React, { Component } from 'react';
-import { FlatList, Text } from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
 import ContentCard from './ContentCard';
 
 type Props = {
@@ -20,13 +20,32 @@ class Content extends Component<Props> {
   _keyExtractor = (item: Item, index: number) => item.id;
 
   _renderItem = ({ item }: Data) => (
-    <ContentCard id={item.id} title={item.title} />
+    <ContentCard id={item.id} title={item.title} backgroundColor={bgColor()} />
   );
 
   render() {
     const { data } = this.props;
-    return <FlatList data={data} renderItem={this._renderItem} />;
+    return (
+      <FlatList
+        data={data}
+        renderItem={this._renderItem}
+        style={styles.container}
+      />
+    );
   }
 }
+
+const bgColor = () => {
+  const colors = ['salmon', 'lightskyblue', 'lightgreen'];
+  return colors[Math.floor(Math.random() * colors.length)];
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: 'lightpink'
+  }
+});
 
 export default Content;
--- src/components/ContentCard.js
+++ src/components/ContentCard.js
@@ -1,17 +1,27 @@
 // @flow
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Platform, Dimensions } from 'react-native';
 
+const screenHeight = Dimensions.get('window').height;
+const isWeb = Platform.OS === 'web';
 type Props = {
   title: string,
-  children?: any
+  children?: any,
+  backgroundColor: string
 };
 
-const ContentCard = ({ title, children }: Props) => (
-  <View>
+const ContentCard = ({ title, children, backgroundColor }: Props) => (
+  <View style={[styles.container, { backgroundColor }]}>
     <Text>{title}</Text>
     {children}
   </View>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    height: Platform.OS === 'web' ? '80vh' : screenHeight * 0.8,
+    marginHorizontal: isWeb ? '10vw' : 0
+  }
+});
 
 export default ContentCard;
